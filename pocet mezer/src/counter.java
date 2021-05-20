75ac819bc7cf15994e2dd21f8188b627f374eb65
Jakub Hrubý <hruby4@SPSEJECNANET.CZ>
2021-05-20 11:19:07 +0200

    zabalení metod do třídy

---
+++ pocet mezer/src/counter.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class counter {
+	int countS= 0;
+	int countW=0;
+	
+	public void CountSW() {
+		
+		try {
+		      File file = new File("lm.txt");
+		      Scanner myReader = new Scanner(file);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        for(int i =0;i<data.length();i++) {
+					
+					if(data.charAt(i)==' ') {
+						countS++;
+					
+					}
+					
+				
+				}
+		        String[] words = data.split("\\s+");
+				
+				countW += words.length;
+		       
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		System.out.println("Count of words is " + countW);
+		System.out.println("Count of spaces is " +countS);
+	}
+}
---
+++ pocet mezer/src/main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+	counter c = new counter();
+	c.CountSW();
+	}
+
+}
